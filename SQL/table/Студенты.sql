f18af21f0fe30d8643cc2cd9c0b160e12dc3b3dc
Anton Volkov <ayvolkov@cloudfactory.ru>
2024-07-01 12:10:39 +0400

    Скрипт таблицы Студенты

---
+++ SQL/table/Студенты.sql
@@ -0,0 +1,7 @@
+if object_id('Студенты') is null
+create table [Студенты](
+	[Идентификатор]	int identity(1,1)	not null,
+	[Имя]			nvarchar(100)		not null,
+	[Фамилия]		nvarchar(100)		not null,
+ )
+go
