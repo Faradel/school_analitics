021631e0572f5fae477946ed79def4a2ae197ddd
Anton Volkov <ayvolkov@cloudfactory.ru>
2024-07-02 23:47:27 +0400

    UTF8

--- SQL/table/Студенты.sql
+++ SQL/table/Студенты.sql
@@ -1,6 +1,6 @@
 if object_id('Студенты') is null
 create table [Студенты](
-	[Идентификатор]	int identity(1,1)	not null,
+	 [Идентификатор]	int identity(1,1)	not null,
 	[Имя]			nvarchar(100)		not null,
 	[Фамилия]		nvarchar(100)		not null,
  )
